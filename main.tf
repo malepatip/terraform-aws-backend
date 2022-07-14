e72f0d1977e3aced7aaea4570d98a93d9bf36f31
Phani Malepati <malephan@amazon.com>
2022-07-14 17:19:35 -0500

    Initial commit

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_s3_bucket" "s3_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorith = "aws:kms"
+        sse_algorithm = "aws:kms"
         kms_master_key_id = aws_kms_key.kms_key.arn
       }
     }
